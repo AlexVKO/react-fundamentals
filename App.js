c5f4a92c70b8b7841f96a608ba8fe54ec630213a
Alex VKO <ale@alexvko.com>
2016-08-30 17:39:11 -0300

    Adds stateless function example

--- App.js
+++ App.js
@@ -2,8 +2,11 @@ import React  from 'react';
 
 class App extends React.Component {
   render() {
-    return <div>Hello</div>
+    return <div>Hello </div>
   }
 }
+
+// Stateless function
+// const App = () =>  <h1> Its the same </h1>
 
 export default App
