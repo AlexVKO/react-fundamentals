d4b56910582184c399b30964e96883c3d3e0172c
Alex VKO <ale@alexvko.com>
2016-08-30 18:01:19 -0300

    Getting params from component props

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React  from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   render() {
-    return <div>Hello </div>
+    return <h1>{this.props.text}</h1>
   }
 }
 
 // Stateless function
 // const App = () =>  <h1> Its the same </h1>
 
-export default App
+// export default App
+
+ReactDOM.render(
+    <App text="this is the props" />,
+    document.getElementById('app')
+  )
