0128b1891cabe5e9c3048455812aed121f352bf8
Alex VKO <ale@alexvko.com>
2016-08-30 18:02:52 -0300

    Uses em6 variable to get the props

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   render() {
-    return <h1>{this.props.text}</h1>
+    let text = this.props.text
+    return <h1>{text}</h1>
   }
 }
 
