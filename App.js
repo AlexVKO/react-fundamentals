152b817768c113a99fa9f2a095486757e4966777
Alex VKO <ale@alexvko.com>
2016-08-30 19:40:54 -0300

    Each instance of the component can be referenced by ref attribute

--- App.js
+++ App.js
@@ -1,42 +1,47 @@
 import React from 'react';
-
+import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor(){
     super();
     this.state = {
-      text: "this is the state text"
+      red: 0,
+      green: 0,
+      blue: 0
     }
 
-    this.update = this.update.bind(this)
+    // this.update = this.update.bind(this)
   }
 
   update(e){
-    this.setState({text: e.target.value})
+    this.setState({
+      red: ReactDOM.findDOMNode(this.refs.red).value,
+      green: ReactDOM.findDOMNode(this.refs.green).value,
+      blue: ReactDOM.findDOMNode(this.refs.blue).value
+    })
   }
 
   render() {
-    let text = this.props.text
+    let text = this.state.text
     return (
-        <div class="">
-          <Widget text={this.state.text} update={this.update}></Widget>
-          <Widget text={this.state.text} update={this.update}></Widget>
-          <Widget text={this.state.text} update={this.update}></Widget>
-          <Widget text={this.state.text} update={this.update}></Widget>
-          <Widget text={this.state.text} update={this.update}></Widget>
-          <Widget text={this.state.text} update={this.update}></Widget>
+        <div>
+          <Slider ref="red" update={this.update.bind(this)} />
+          <h1>{this.state.red}</h1>
+          <Slider ref="green" update={this.update.bind(this)} />
+          <h1>{this.state.green}</h1>
+          <Slider ref="blue" update={this.update.bind(this)} />
+          <h1>{this.state.blue}</h1>
         </div>
     )
   }
 
 }
 
-const Widget = (props) => {
-  return (
-            <div>
-              <input type="text" onChange={props.update} />
-              <h1>{props.text}</h1>
-            </div>
-          )
+class Slider extends React.Component {
+  render() {
+    return (
+      <input type="range" min="0" max="255" onChange={this.props.update} />
+    )
+  }
 }
 
 App.propTypes = {
