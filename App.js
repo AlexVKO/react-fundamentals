afb432ba6146d504feb267e808a7e12e7450ed02
Alex VKO <ale@alexvko.com>
2016-08-30 18:48:38 -0300

    Simulates two day data-binding of angularjs

--- App.js
+++ App.js
@@ -1,15 +1,31 @@
-import React  from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      text: "this is the state text"
+    }
+  }
+
+  update(e){
+    this.setState({text: e.target.value})
+  }
+
   render() {
     let text = this.props.text
-    return <h1>{text}</h1>
+    return (
+            <div>
+              <input type="text" onChange={this.update.bind(this)} />
+              <h1>{this.state.text}</h1>
+            </div>
+    )
   }
+
 }
 
 App.propTypes = {
-  txt: React.PropTypes.string,
+  text: React.PropTypes.string,
   cat: React.PropTypes.number.isRequired
 }
 
@@ -17,12 +33,5 @@ App.defaultProps = {
   text: "this is the default value of props"
 }
 
-ReactDOM.render(
-    <App cat={5}/>,
-    document.getElementById('app')
-  )
-// Stateless function
-// const App = () =>  <h1> Its the same </h1>
-
-// export default App
+export default App
 
